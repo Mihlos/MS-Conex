a17e355234e3449de63df578f1ad0fad0280c483
Mihlos <miguel.mish@gmail.com>
2018-10-09 21:27:46 +0200

    con el js de leer los clientes funcionando y añadidos al html

--- scripts/prueba.js
+++ scripts/prueba.js
@@ -1,6 +1,8 @@
 const readCostumer = document.getElementById('BtnReadCostumer')
 const readArticles = document.getElementById('BtnReadArticles')
 
+const costumerTable = document.getElementById('costumers-table')
+
 // const mostrar = e => {
 //      console.log(e.target.innerText)
 //  }
@@ -12,23 +14,38 @@ const readArticles = document.getElementById('BtnReadArticles')
 //      mostrar(e)
 //  })
 
-readCostumer.addEventListener('click', () => {
-    var xhr= new XMLHttpRequest();
-    var url= 'http://manchasoft.ddns.net:4325/MCS/dbGestion_dat/v1/MG_ENTIDAD?api_key=123456';
+readCostumer.addEventListener('click', evt => {
+    const xhr= new XMLHttpRequest()
+    var url= 'http://manchasoft.ddns.net:4325/MCS/dbGestion_dat/v1/MG_ENTIDAD?api_key=123456'
+    xhr.open('GET', url, true)
     
-    xhr= new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.send();
-    
-    if ( (xhr.errorCode==0) && (xhr.status == 200) ) {
-        var resultado= xhr.response;
-        var oJSON= JSON.parse(resultado);
+    xhr.addEventListener('load', e => {
+        var resultado = e.target.response
+        var oJSON= JSON.parse(resultado)
         var totalLista = oJSON.total_count
-    
+
         for ( nReg = 0; nReg < totalLista; nReg++ ) {
-            var oRegistro= oJSON.mg_entidad[nReg];
+            var oRegistro= oJSON.mg_entidad[nReg]
             var nomCli= oRegistro.name
-            console.log(nomCli)
-        }
-    }
+            
+            //Añadir al HTML principal
+            var cliente = document.createElement('p')
+            cliente.textContent = nomCli
+
+            costumerTable.appendChild(cliente)
+        } 
+    })
+    xhr.send()
+    
+    // if ( (xhr.errorCode==0) && (xhr.status == 200) ) {
+    //     var resultado= xhr.response;
+    //     var oJSON= JSON.parse(resultado);
+    //     var totalLista = oJSON.total_count
+    
+    //     for ( nReg = 0; nReg < totalLista; nReg++ ) {
+    //         var oRegistro= oJSON.mg_entidad[nReg];
+    //         var nomCli= oRegistro.name
+    //         console.log(nomCli)
+    //     }
+    // }
 })
